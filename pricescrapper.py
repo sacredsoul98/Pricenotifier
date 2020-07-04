cf907879f2554992f3811b4661ca336057e3b400
Surya P <suryapilaka@gmail.com>
2020-07-04 20:24:35 +0530

    removed mail feature

--- pricescrapper.py
+++ pricescrapper.py
@@ -10,61 +10,28 @@ headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Geck
 def check_price():
     page= requests.get(url, headers=headers)
 
-    soup1=BeautifulSoup(page.content, 'html.parser')
+    soup=BeautifulSoup(page.content, 'html.parser')
 
-    soup2=BeautifulSoup(soup1.prettify(),"html.parser")
+    #soup2=BeautifulSoup(soup1.prettify(),"html.parser")
 
     #print(soup.prettify())
-
-    title=soup2.find(id='productTitle').get_text()
+  
+    title=soup.find(id="productTitle").get_text()
+    print(title)
 
     print(title.strip())
 
-    price=soup2.find(id='priceblock_ourprice').get_text()
+    price=soup.find(id='priceblock_ourprice').get_text()
 
     print(price)
 
-    converted_price=float(price[1:5])  #The problem occurs here
+    converted_price=float(price[4:10])  #The problem occurs here
 
 
     wanted_price=3,000  #the price u need
     if(converted_price<=3,000):
-        print("starting mail service")
-        sendmail()
-
-
-
-def sendmail():
-    
-    #The username and password  are removed and should be added before execution
-
-    username='sender email'
-    password='sender password'
-    subject="price fell down"
-
-    content='The price has decreased'
-    msg= f'Subject:{subject}\n\n{content}'
-
-    #init gail SMTP
-    mail= smtplib.SMTP('smtp.gmail.com',587)
-
-    #identify to server
-
-    mail.ehlo()
-
-    #encrypt session
-    mail.starttls()
-
-    #login
-    mail.login('username','password')
-
-    #send message
-    mail.sendmail('Sender email ','Reciever email',msg)
-
-    #end mail connection 
-    mail.close()
-
-    print('Email Sent')
+        print("U can buy now")
+        
 
 
 
